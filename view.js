bd5f282be8cd05560677fe3ef2270eafd42bee2d
dasun <hmdtharaka@gmail.com>
2020-03-29 16:07:02 +0530

    Counter Example

--- view.js
+++ view.js
@@ -1,2 +1,9 @@
 var greeter = document.getElementById("greeter");
+var countText = document.getElementById("click-counter");
 greeter.innerText = "Hello world!";
+var button = document.getElementById("countbtn");
+var count = 0;
+button.onclick = function () {
+    countText.innerText = count.toString();
+    count++;
+};
--- view.ts
+++ view.ts
@@ -1,3 +1,13 @@
 let greeter:HTMLHeadingElement = document.getElementById("greeter") as HTMLHeadingElement;
 
 greeter.innerText = "Hello world!";
+
+var button =  document.getElementById("countbtn");
+var count:number = 0;
+
+
+button.onclick = function() {  
+    let countText:HTMLHeadingElement = document.getElementById("click-counter") as HTMLHeadingElement;
+    countText.innerText = count.toString();
+    count++;
+}
