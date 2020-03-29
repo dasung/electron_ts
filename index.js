c3a4762b5c56cdc11173fd469841c89d4e2f0f43
dasun <hmdtharaka@gmail.com>
2020-03-29 08:42:16 +0530

    Open the DevTools for debugging

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
+
+  // Open the DevTools for debugging
+  // win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
