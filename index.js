f2aa7cc1c309c980ee4e9141b959d55985ae1e8c
dasun <hmdtharaka@gmail.com>
2020-03-29 10:10:57 +0530

    Called clean up

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ function createWindow () {
 }
 
 app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
